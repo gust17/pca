ad4d4be766b8fd12ef36f8b60cfcf49d260e0418
Aureane Moraes <aureane.moraes@outlook.com>
2022-08-05 13:14:48 -0300

    criado arquivo de helpers e funcoes de upload e remove img

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,23 @@
+<?php
+if (! function_exists('uploadImg')) {
+    function uploadImg($file, $path)
+    {
+        $imageName = $file->getClientOriginalName() . time().'.'.$file->extension();  
+
+        $file->move(public_path($path), $imageName);
+
+        $fullName = $path . '/' . $imageName;
+        
+        return $fullName;
+    }
+}
+
+if (! function_exists('removeImg')) {
+    function removeImg($fullName)
+    {
+        unlink($fullName);
+
+        return true;
+    }
+}
+  
